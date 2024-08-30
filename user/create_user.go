3d5ff53dd759295b31904f19112af89a83dfb796
Pablo Braulio <pablo_braulio.escriba@mail.schwarz>
2024-08-30 08:25:45 +0200

    Add find user into create user.

--- user/create_user.go
+++ user/create_user.go
@@ -2,13 +2,23 @@ package user
 
 import (
 	"context"
+	"errors"
 	"github.com/google/uuid"
 	"github.com/upper/db/v4"
 )
 
+var ErrUserAlreadyExists = errors.New("user already exists")
+
 const UsersTableName = "users"
 
 func CreateUser(ctx context.Context, session db.Session, u PostgresUser) error {
+	current, err := FindUser(ctx, session, u.ID)
+	if err != nil {
+		return err
+	}
+	if current != nil {
+		return ErrUserAlreadyExists
+	}
 	if _, err := session.WithContext(ctx).Collection(UsersTableName).Insert(u); err != nil {
 		return err
 	}
